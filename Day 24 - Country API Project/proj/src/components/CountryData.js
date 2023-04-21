4483c9db907ac2887aded1fdab37960bf66ed40c
Adnan Bhanji <bhanjiadnan786@gmail.com>
2023-04-21 15:57:44 +0200

    Day 24 - Country API Project Done

--- Day 24 - Country API Project/proj/src/components/CountryData.js
+++ Day 24 - Country API Project/proj/src/components/CountryData.js
@@ -7,49 +7,55 @@ const CountryData = ({ respData }) => {
       {respData.map((country) => {
         return (
           <div
-            key={country.name.official}
-            className="mt-4 mb-4 w-96 h-96 flex flex-col justify-center items-center"
+            key={country.name.common}
+            className="mt-4 mb-4 w-96 h-96 flex flex-col justify-center items-center bg-slate-100 ml-2 mr-2 pt-8"
           >
             {" "}
             {/* add a class to each country's container div */}
-            <h2>{country.name.official}</h2>
-            <div className="w-72 h-54 ">
+            <div className="w-64 h-48 flex justify-center items-center ">
               <img
                 src={country.flags.png}
                 alt=""
-                className="w-full h-48 object-cover"
+                className="w-full h-32 object-cover"
               />
             </div>
-            <h2>Capital: {country.capital}</h2>
-            <h2>Population: {country.population}</h2>
-            <h2>Currency:</h2>
-            {country.currencies && // we cannot use map here because its not an array
-              // but its an object. so what we do instead is check if the value exists,
-              // and then use map but on the values, which is an array of values.
-              // so now our map is functional.
-              Object.values(country.currencies)
-                .slice(0, 1)
-                .map((curr) => {
-                  return (
-                    <div key={Math.random()}>
-                      <li>
-                        <span>{curr.symbol}</span> - {curr.name}
-                      </li>
-                    </div>
-                  );
-                })}
-            <h2>Languages:</h2>
-            {country.languages && // we cannot use map here because its not an array
-              // but its an object. so what we do instead is check if the value exists,
-              // and then use map but on the values, which is an array of values.
-              // so now our map is functional.
-              Object.values(country.languages)
-                .slice(0, 2)
-                .map((lang) => {
-                  return <li key={Math.random()}>{lang}</li>;
-                })}
-            <br />
-            <br />
+            <div>
+              <h2 className="text-yellow-500 text-2xl p-2 break-words">
+                {country.name.official}
+              </h2>
+              <h2 className="p-1">Capital: {country.capital}</h2>
+              <h2 className="p-1">Population: {country.population}</h2>
+              <h2 className="p-1">
+                Currency:{" "}
+                {country.currencies && // we cannot use map here because its not an array
+                  // but its an object. so what we do instead is check if the value exists,
+                  // and then use map but on the values, which is an array of values.
+                  // so now our map is functional.
+                  Object.values(country.currencies)
+                    .slice(0, 1)
+                    .map((curr) => {
+                      return (
+                        <span key={Math.random()}>
+                          {curr.symbol} - {curr.name}
+                        </span>
+                      );
+                    })}
+              </h2>
+              <h2 className="p-1">
+                Languages:{" "}
+                {country.languages && // we cannot use map here because its not an array
+                  // but its an object. so what we do instead is check if the value exists,
+                  // and then use map but on the values, which is an array of values.
+                  // so now our map is functional.
+                  Object.values(country.languages)
+                    .slice(0, 2)
+                    .map((lang) => {
+                      return <span key={Math.random()}>{lang}</span>;
+                    })}
+              </h2>
+              <br />
+              <br />
+            </div>
           </div>
         );
       })}
