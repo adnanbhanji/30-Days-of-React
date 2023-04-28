13dd1fe053fca972a5e5b937d325ed8371a316d6
Adnan Bhanji <bhanjiadnan786@gmail.com>
2023-04-28 17:12:09 +0200

    Day 30 - Conclusion Done

---
+++ Day 30 - Conclusion/completion_interface/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ReactPic from "./React.png";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Completed React 30 Day Challenge</h1>
+      <img src={ReactPic} alt="Description of your image" />
+    </div>
+  );
+}
+
+export default App;
